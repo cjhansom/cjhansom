84d5cf718b341d737755184b49ff67a7c982a616
cjhansom <46542011+cjhansom@users.noreply.github.com>
2019-01-12 10:51:55 +0100

    Add files via upload

---
+++ house.v
@@ -0,0 +1,32 @@
+ hello friend my name is innocent i am learning how to program and it has being a very nice expirience, let
+me tell you more about it.
+frist we were thought about what computer really is and it apears we use computers litrally every day. and some computers are 
+are machanical while others are not but they all carryout a particular purpose  which involves accepting data processing it
+and giving back the result as output or information. lets talk about one of this machanical type of computer the CURTA.
+
+ WHAT IT LOOKS LIKE;
+the curta is a mechanical machine invented in 1930s by  Curt Herzstark.
+ To me it looks like a small win drum with a liver because it has the same shape like one,(or perhaps a paper grander)  just 
+that it has a kind of stire rod at the top that when turned tourchs the inner chambers of the numbers that appears at the top
+of it thereby causing it to change.it has no batries, numbers ranging from zero to nine is found at the top. and something like a 
+pin is also found at the top. and at the round up to botom of it are also numbers and botton like objects that moves up and down.
+
+ THE KIND OF DATA IT ACCEPTS;
+The CURTA is a calculating machine that deals with add of numbers, multipling of numbers and subtracting of numbers. put simply its 
+a calculator.
+
+ HOW IT WORKS;
+the stire liver run down at the middle of the cylinder and it also has teeth that is connected to the result chamber. so if any of this
+teeth is affected it simply turns round thereby changing the possition of the chamber and affecting the result and thats why we move 
+the side botton to affect the disired number which then affects our result. 
+
+ MULTIPLCATION
+The CURTA makes it easy to calculate numbers in a very facinating way, and infact easy way. let take for instance we want to matiply 
+four-hundred by three we simply move the fouth botton at the side of it down to four while the rest are at zero and then turn the 
+stire botton at the top clock wise three times and there you go the result appears at the top of it.
+
+ STRATION
+Straction is also very easy we just lift up the liver we used to stire a bit and the red line apears at the liver then we turn again 
+and it simply subtracts we could also turn the top head to the numbers one which means maltiply by ten, when its at two it means multiply
+by two tens and so on.
+  
